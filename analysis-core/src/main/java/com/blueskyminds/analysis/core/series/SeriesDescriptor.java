53401cafc038cf7ac870e89e8e531d58c89cf244
jeromyevans <jeromy.evans@blueskyminds.com.au>
2007-12-23 00:13:41 +0000

    Added missing SeriesDescriptor

---
+++ analysis-core/src/main/java/com/blueskyminds/analysis/core/series/SeriesDescriptor.java
@@ -0,0 +1,15 @@
+package com.blueskyminds.analysis.core.series;
+
+/**
+ *
+ * Uniquely identifies the series/sample used the unique identifier for a Series.  The series metadata
+ *
+ * Date Started: 24/08/2006
+ *
+ * History:
+ *
+ * ---[ Blue Sky Minds Pty Ltd ]------------------------------------------------------------------------------
+ */
+public interface SeriesDescriptor {
+
+}
